2e894bd1f9f3a7718bb2aaf8ed7a5087900d1e07
채정훈 <wjdgns4035@iabacus.co.kr>
2024-02-27 14:18:01 +0900

    router and layout setting

---
+++ src/pages/Header.tsx
@@ -0,0 +1,16 @@
+import "~/styles/header.css";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div>
+        <h1>menu</h1>
+      </div>
+      <div>
+        <h1>items-Area</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
